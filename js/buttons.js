08b43d823f8f2557375f67ff4aaf135db56c4314
Stanislav Shastov <shastov.stas1991@gmail.com>
2024-11-25 02:05:46 +0300

    ++

---
+++ js/buttons.js
@@ -0,0 +1,6 @@
+export function uploadImage() {
+    const buttonUpload = document.querySelector('.button__upload');
+    buttonUpload.addEventListener('click', () => {
+        console.log('Это кнопка загрузки');
+    });
+}
--- js/main.js
+++ js/main.js
@@ -1,11 +1,9 @@
 import Cropper from 'cropperjs';
+import { uploadImage } from './buttons.js';
 
 const image  = document.getElementById('image');
 const cropper = new Cropper(image, {
     aspectRatio: 16 / 9
 });
 
-const uploadButton = document.querySelector('.upload-btn');
-uploadButton.addEventListener('click', () => {
-  console.log(image.src);
-});
+uploadImage();
